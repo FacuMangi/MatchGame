af09c48300e5018b672bbf48af7e6ca0b4f2e469
Facundo Mangioni <106379788+FacuMangi@users.noreply.github.com>
2023-06-07 18:48:47 -0300

    Juego terminado.

--- MatchGame/MatchGame/MainWindow.xaml.cs
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -15,25 +15,42 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
+    using System.Windows.Threading;
+
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again? ;)";
+            }
         }
 
         private void SetUpGame()
         {
             List<string> emoji = new List<string>()
             {
-                "🎈","🎈",
+                "(⊙_⊙;)","(⊙_⊙;)",
                 "ಠ_ಠ","ಠ_ಠ",
                 "ಥ_ಥ","ಥ_ಥ",
-                "👙","👙",
+                "(•_•)","(•_•)",
                 "(⌐■_■)","(⌐■_■)",
                 "(* ω *)","(* ω *)",
                 "💣","💣",
@@ -44,12 +61,52 @@ namespace MatchGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(emoji.Count);
-                string nextEmoji = emoji[index];
-                textBlock.Text = nextEmoji;
-                emoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    int index = random.Next(emoji.Count);
+                    string nextEmoji = emoji[index];
+                    textBlock.Text = nextEmoji;
+                    emoji.RemoveAt(index);
+                }      
             }
-        }   
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; 
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
     }                 
 }                     
                       
